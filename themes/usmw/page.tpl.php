93d728284a93161346409106e6bbb9b0646ec238
aleverenz <leverenz@uta.edu>
2018-08-21 09:07:49 -0500

    Revert "add back drupal main menu"
    This reverts commit abfafd3e992dc31d0aeb10fca98451860cf34dac.

--- themes/usmw/page.tpl.php
+++ themes/usmw/page.tpl.php
@@ -122,13 +122,7 @@
     <div id="cssmenu">
     </div>
     
-       <?php if ($main_menu || $secondary_menu): ?>
-      <div id="navigation"><div class="section">
-        <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-        
-        <?php /* print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); */ ?>
-      </div></div> <!-- /.section, /#navigation -->
-    <?php endif; ?>
+    
     
     
     <br class="clearfloat">
