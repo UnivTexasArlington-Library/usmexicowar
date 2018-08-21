10145da2843540b3f6e5b7c073a105a644def7c9
aleverenz <leverenz@uta.edu>
2018-08-21 09:07:38 -0500

    Revert "menu css"
    This reverts commit a19afc4170a18f3c7d79091a57279e7d2fa939c3.

--- themes/usmw/page.tpl.php
+++ themes/usmw/page.tpl.php
@@ -120,14 +120,16 @@
     <br class="clearfloat header_logo_clear">
 
     <div id="cssmenu">
-         <?php if ($main_menu || $secondary_menu): ?>
+    </div>
+    
+       <?php if ($main_menu || $secondary_menu): ?>
       <div id="navigation"><div class="section">
         <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
         
         <?php /* print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); */ ?>
       </div></div> <!-- /.section, /#navigation -->
     <?php endif; ?>
-   </div> 
+    
     
     <br class="clearfloat">
 
